e4641ffe2ade3f7733d856eace14dc243aee539c
markchil <mark.chilenski@gmail.com>
2013-09-27 16:29:43 -0400

    Corrected selection of FRC data.

--- demo/gptools_mds.py
+++ demo/gptools_mds.py
@@ -69,8 +69,10 @@ for k in xrange(0, 32):
     R_mid_FRC.append(N_R.data())
 # Assume all slow channels are on the same timebase:
 t_FRC = N.dim_of().data()
-Te_FRC = scipy.asarray(Te_FRC, dtype=float)
-R_mid_FRC = scipy.asarray(R_mid_FRC, dtype=float)
+ok_idxs = (t_FRC >= flat_start) & (t_FRC <= flat_stop)
+t_FRC = t_FRC[ok_idxs]
+Te_FRC = scipy.asarray(Te_FRC, dtype=float)[:, ok_idxs]
+R_mid_FRC = scipy.asarray(R_mid_FRC, dtype=float)[:, ok_idxs]
 
 # Get magnetic axis location:
 R_mag = efit_tree.getMagR()
