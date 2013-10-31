8bd527ab2329866a4e82983e096138926a4eab90
markchil <mark.chilenski@gmail.com>
2013-10-31 12:38:03 -0400

    Take NTH samples off ne plot.

--- demo/gptools_mds_ne.py
+++ demo/gptools_mds_ne.py
@@ -345,7 +345,7 @@ a1.plot(Rstar, mean, 'k', linewidth=3, label='mean')
 a1.fill_between(Rstar, mean-std, mean+std, alpha=0.375, facecolor='k')
 a1.plot(Rstar, mean_nth, 'g', linewidth=3, label='NTH')
 a1.fill_between(Rstar, mean_nth-std_nth, mean_nth+std_nth, alpha=0.375, facecolor='g')
-a1.plot(Rstar, ne_nth)
+# a1.plot(Rstar, ne_nth)
 a1.errorbar(R_mid_w, ne_TS_w, xerr=dev_R_mid_w, yerr=dev_ne_TS_w, fmt='r.', label='CTS') # 
 a1.errorbar(R_mid_ETS_w, ne_ETS_w, xerr=dev_R_mid_ETS_w, yerr=dev_ne_ETS_w, fmt='m.', label='ETS') # 
 a1.axvline(x=R_mag_mean, color='r', label='$R_{mag}$')
