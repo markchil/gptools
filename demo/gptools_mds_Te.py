07a037c9ce6ff328a27bbca52cb5498bab4c934b
markchil <mark.chilenski@gmail.com>
2013-11-02 15:12:13 -0400

    Put NTH Te fit on correct grid, change labels.

--- demo/gptools_mds_Te.py
+++ demo/gptools_mds_Te.py
@@ -281,7 +281,7 @@ nk = gptools.DiagonalNoiseKernel(1, n=0, initial_noise=0.0, fixed_noise=True, no
 gp = gptools.GaussianProcess(k, noise_k=nk)
 gp.add_data(R_mid_w, Te_TS_w, err_y=dev_Te_TS_w)
 gp.add_data(R_mid_ETS_w, Te_ETS_w, err_y=dev_Te_ETS_w)
-# gp.add_data(R_mid_FRC_w, Te_FRC_w, err_y=dev_Te_FRC_w)
+gp.add_data(R_mid_FRC_w, Te_FRC_w, err_y=dev_Te_FRC_w)
 gp.add_data(R_mid_GPC2_w, Te_GPC2_w, err_y=dev_Te_GPC2_w)
 gp.add_data(R_mid_GPC_w, Te_GPC_w, err_y=dev_Te_GPC_w)
 gp.add_data(R_mag_mean, 0, n=1)
@@ -347,8 +347,8 @@ opt_elapsed = time.time() - opt_start
 # Rstar = scipy.linspace(0.63, 0.93, 24*30)
 fits_file = scipy.io.readsav('/home/markchil/codes/gptools/demo/nth_samples_1101014006.save')
 # Average over the inexplicably shifting Rmajor array:
-# Rstar = scipy.mean(fits_file.te_fit.rmajor[0][:, 32:72], axis=1)
-Rstar = fits_file.ne_fit.rmajor[0][:, 0]
+Rstar = scipy.mean(fits_file.te_fit.rmajor[0][:, 32:72], axis=1)
+# Rstar_nth = fits_file.ne_fit.rmajor[0][:, 0]
 Te_nth = fits_file.te_fit.te_comb_fit[0][:, 32:72]
 mean_nth, std_nth = gptools.compute_stats(Te_nth, robust=robust)
 
@@ -378,9 +378,9 @@ f.suptitle('Univariate GPR on time-averaged data')
 # f.suptitle('With slope constraint')
 
 a1 = f.add_subplot(3, 1, 1)
-a1.plot(Rstar, mean, 'k', linewidth=3, label='mean')
+a1.plot(Rstar, mean, 'k', linewidth=3, label='GPR')
 a1.fill_between(Rstar, mean-std, mean+std, alpha=0.375, facecolor='k')
-a1.plot(Rstar, mean_nth, 'g', linewidth=3, label='NTH')
+a1.plot(Rstar, mean_nth, 'g', linewidth=3, label='spline')
 a1.fill_between(Rstar, mean_nth-std_nth, mean_nth+std_nth, alpha=0.375, facecolor='g')
 # a1.plot(Rstar, Te_nth)
 a1.errorbar(R_mid_w, Te_TS_w, xerr=dev_R_mid_w, yerr=dev_Te_TS_w, fmt='r.', label='CTS')
