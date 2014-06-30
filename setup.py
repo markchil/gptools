25b43e143d6a4bda8d2c4df62d317b2c7e98429d
markchil <mark.chilenski@gmail.com>
2014-06-30 15:41:03 -0400

    More work on getting ready to distribute.

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='gptools',
+    version='0.1',
+    packages=['gptools', 'gptools.kernel'],
+    requires=['scipy', 'numpy', 'matplotlib', 'mpmath', 'emcee', 'triangle'],
+    author='Mark Chilenski',
+    author_email='mark.chilenski@gmail.com',
+    url='https://github.com/markchil/gptools',
+    description='Gaussian process regression with derivative constraints and predictions.',
+    long_description=open('README', 'r').read(),
+    license='GPL'
+)
