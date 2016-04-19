d643c953a0ca08a881d4da62ed39f2bcd1206591
markchil <mark.chilenski@gmail.com>
2016-04-19 14:08:31 -0400

    Added support for hyperparameter derivatives.

--- gptools/kernel/squared_exponential.py
+++ gptools/kernel/squared_exponential.py
@@ -24,9 +24,12 @@ from .core import Kernel
 
 import scipy
 import scipy.special
+import warnings
 
 class SquaredExponentialKernel(Kernel):
     r"""Squared exponential covariance kernel. Supports arbitrary derivatives.
+    
+    Supports derivatives with respect to the hyperparameters.
     
     The squared exponential has the following hyperparameters, always
     referenced in the order listed:
@@ -95,36 +98,56 @@ class SquaredExponentialKernel(Kernel):
         -------
         Kij : :py:class:`Array`, (`M`,)
             Covariances for each of the `M` `Xi`, `Xj` pairs.
-        
-        Raises
-        ------
-        NotImplementedError
-            If hyper_deriv is not None and `n` > 0.
         """
-        only_first_order = ((scipy.asarray(ni, dtype=int) == 0).all() and
-                            (scipy.asarray(nj, dtype=int) == 0).all())
-        if hyper_deriv is not None and not only_first_order:
-            raise NotImplementedError("Hyperparameter derivatives with n > 0 "
-                                      "have not been implemented!")
+        only_first_order = (
+            (scipy.asarray(ni, dtype=int) == 0).all() and
+            (scipy.asarray(nj, dtype=int) == 0).all()
+        )
         tau = scipy.asarray(Xi - Xj, dtype=float)
         r2l2, l_mat = self._compute_r2l2(tau, return_l=True)
         k = self.params[0]**2 * scipy.exp(-r2l2 / 2.0)
-        # Account for derivatives:
-        # Get total number of differentiations:
-        n_tot_j = scipy.asarray(scipy.sum(nj, axis=1), dtype=int).flatten()
-        n_combined = scipy.asarray(ni + nj, dtype=int)
-        # Compute factor from the dtau_d/dx_d_j terms in the chain rule:
-        j_chain_factors = (-1.0)**(n_tot_j)
-        # Compute Hermite polynomial factor:
-        hermite_factors = scipy.prod((-1.0 / (scipy.sqrt(2.0) * l_mat))**(n_combined) *
-                                     scipy.special.eval_hermite(n_combined, tau / (scipy.sqrt(2.0) * l_mat)),
-                                     axis=1)
-        
-        k = j_chain_factors * hermite_factors * k
+        if not only_first_order:
+            # Account for derivatives:
+            # Get total number of differentiations:
+            n_tot_j = scipy.asarray(scipy.sum(nj, axis=1), dtype=int).flatten()
+            n_combined = scipy.asarray(ni + nj, dtype=int)
+            # Compute factor from the dtau_d/dx_d_j terms in the chain rule:
+            j_chain_factors = (-1.0)**(n_tot_j)
+            # Compute Hermite polynomial factor:
+            hermite_factors = (
+                (-1.0 / (scipy.sqrt(2.0) * l_mat))**(n_combined) *
+                scipy.special.eval_hermite(n_combined, tau / (scipy.sqrt(2.0) * l_mat))
+            )
+            # Handle length scale hyperparameter derivatives:
+            if hyper_deriv is not None and hyper_deriv > 0:
+                t = (tau[:, hyper_deriv - 1])**2.0 / (self.params[hyper_deriv])**3.0
+                mask = n_combined[:, hyper_deriv - 1] > 0
+                t[mask] -= n_combined[mask, hyper_deriv - 1]  / self.params[hyper_deriv]
+                mask = mask & (tau[:, hyper_deriv - 1] != 0.0)
+                t[mask] -= (
+                    scipy.sqrt(2.0) * n_combined[mask, hyper_deriv - 1] *
+                    tau[mask, hyper_deriv - 1] / (self.params[hyper_deriv])**2.0 *
+                    scipy.special.eval_hermite(
+                        n_combined[mask, hyper_deriv - 1] - 1,
+                        tau[mask, hyper_deriv - 1] / (scipy.sqrt(2.0) * self.params[hyper_deriv])
+                    ) /
+                    scipy.special.eval_hermite(
+                        n_combined[mask, hyper_deriv - 1],
+                        tau[mask, hyper_deriv - 1] / (scipy.sqrt(2.0) * self.params[hyper_deriv])
+                    )
+                )
+                hermite_factors[:, hyper_deriv - 1] *= t
+            
+            k = j_chain_factors * scipy.prod(hermite_factors, axis=1) * k
         # Take care of hyperparameter derivatives:
         if hyper_deriv is None:
             return k
         elif hyper_deriv == 0:
-            return 2 * k / self.params[0]
+            return 2.0 * k / self.params[0] if self.params[0] != 0.0 else scipy.zeros_like(k)
         else:
-            return (tau[:, hyper_deriv - 1])**2 / (self.params[hyper_deriv - 1])**3 * k
+            # Keep efficient form for only_first_order:
+            if only_first_order:
+                return (tau[:, hyper_deriv - 1])**2.0 / (self.params[hyper_deriv])**3.0 * k
+            else:
+                # Was already computed above:
+                return k
